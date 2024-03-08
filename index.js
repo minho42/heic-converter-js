0f5b4fd0d9df8775cfe1662b6c540fc8bf447197
Min ho Kim <minho42@gmail.com>
2024-03-09 00:27:40 +1100

    uu

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-const { program } = require("commander");
-const { promisify } = require("util");
-const heicConvert = require("heic-convert");
+const fs = require("fs")
+const path = require("path")
+const { program } = require("commander")
+const { promisify } = require("util")
+const heicConvert = require("heic-convert")
 
 const convert = async (filename, quality) => {
-  const inputBuffer = await promisify(fs.readFile)(filename);
+  const inputBuffer = await promisify(fs.readFile)(filename)
   const outputBuffer = await heicConvert({
     buffer: inputBuffer,
     format: "JPEG",
     quality,
-  });
+  })
 
-  const basePath = path.dirname(filename);
-  const name = path.basename(filename, path.extname(filename));
-  const newFilename = path.join(basePath, `${name}.jpg`);
+  const basePath = path.dirname(filename)
+  const name = path.basename(filename, path.extname(filename))
+  const newFilename = path.join(basePath, `${name}.jpg`)
 
-  await promisify(fs.writeFile)(newFilename, outputBuffer);
-};
+  await promisify(fs.writeFile)(newFilename, outputBuffer)
+}
 
-program.option("-f, --filename <char>").option("-q, --quality <char>", "quality", "0.5");
-program.parse();
+program.option("-f, --filename <char>").option("-q, --quality <char>", "quality", "0.5")
+program.parse()
 
-convert(program.opts().filename, program.opts().quality);
+const options = program.opts()
+convert(options.filename, options.quality)
