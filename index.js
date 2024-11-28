d14b8f71afbd36b151b799ae2210fb6d3be41c73
Min ho Kim <minho42@gmail.com>
2024-11-28 22:59:53 +1100

    catch exception; handle if input file is not heic

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const { promisify } = require("util")
 const heicConvert = require("heic-convert")
 
 const convert = async (filename, quality) => {
-  const inputBuffer = await promisify(fs.readFile)(filename)
-  const outputBuffer = await heicConvert({
-    buffer: inputBuffer,
-    format: "JPEG",
-    quality,
-  })
+  try {
+    const ext = path.extname(filename).toLowerCase()
+    if (ext !== ".heic") {
+      console.log(`not a heic file: *${ext}`)
+      return
+    }
 
-  const basePath = path.dirname(filename)
-  const name = path.basename(filename, path.extname(filename))
-  const newFilename = path.join(basePath, `${name}.jpg`)
+    const inputBuffer = await promisify(fs.readFile)(filename)
+    const outputBuffer = await heicConvert({
+      buffer: inputBuffer,
+      format: "JPEG",
+      quality,
+    })
 
-  await promisify(fs.writeFile)(newFilename, outputBuffer)
+    const basePath = path.dirname(filename)
+    const name = path.basename(filename, path.extname(filename))
+    const newFilename = path.join(basePath, `${name}.jpg`)
+
+    await promisify(fs.writeFile)(newFilename, outputBuffer)
+  } catch (error) {
+    console.log(`error converting ${filename}: `, error.message)
+  }
 }
 
 program.option("-f, --filename <char>").option("-q, --quality <char>", "quality", "0.5")
